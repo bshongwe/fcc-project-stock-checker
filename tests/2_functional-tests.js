3cd817284d06e423ce4fba24dda76dfac8570626
bshongwe <dru.da.beat.hustle@gmail.com>
2024-12-24 14:38:46 +0000

    Stock data GET Tests: Stock symbol >> string, price >> num, likes >> num

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,30 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  // GET Test: request to /api/stock-prices with a NASDAQ stock symbol
+  test('GET /api/stock-prices with stock symbol should return stockData with symbol, price, and likes', function(done) {
+    const stockSymbol = 'AAPL';
+
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ symbol: stockSymbol })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.isObject(res.body.stockData);
+
+        // stockData Check: 'symbol', 'price', and 'likes'
+        assert.property(res.body.stockData, 'symbol');
+        assert.isString(res.body.stockData.symbol);
+
+        assert.property(res.body.stockData, 'price');
+        assert.isNumber(res.body.stockData.price);
+
+        assert.property(res.body.stockData, 'likes');
+        assert.isNumber(res.body.stockData.likes);
+
+        done();
+      });
+  });
+
 });
+
