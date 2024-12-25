4c7ec7df6176ec6d4ae331f9656729be23a2edd0
Ernest Bhekizwe Shongwe <dru.da.beat.hustle@gmail.com>
2024-12-25 02:15:49 +0000

    Updated tests 2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,7 +1,7 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
-const server = require('../server'); // Adjust path if necessary
+const server = require('../server');
 
 chai.use(chaiHttp);
 
